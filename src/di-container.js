509496fa8f52a84880572e918a48f8d0fc085f2d
themockingjester <yashmathur123123@gmail.com>
2025-01-18 01:38:16 +0530

    added support for logger using winston

--- src/di-container.js
+++ src/di-container.js
@@ -1,10 +1,11 @@
 // this file is to fullfill the purpose of dependency injection
 const config = require("../config.json")
-
+const { logger } = require("./utils/logger")
 const DI_CONTAINER = {
     config: config,
     databaseHandler: null,
     dbModels: null,
+    logger: logger,
     messageChannels: {
         kafka: {
             producer: null,
