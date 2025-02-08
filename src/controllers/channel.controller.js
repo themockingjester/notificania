3b5d136c2b392023636664e6ab15fc805adce0e3
themockingjester <yashmathur123123@gmail.com>
2025-02-08 23:11:06 +0530

    added necessary logs

--- src/controllers/channel.controller.js
+++ src/controllers/channel.controller.js
@@ -1,19 +1,38 @@
-const { HTTP_CODES } = require("../constants/httpCodes.constant")
-const { SERVER_RESPONSES } = require("../constants/serverResponse.constant")
-const ChannelService = require("../services/channel.services")
-const { generateResponse } = require("../utils/common.utils")
+const { HTTP_CODES } = require("../constants/httpCodes.constant");
+const { SERVER_RESPONSES } = require("../constants/serverResponse.constant");
+const { logger } = require("../di-container");
+const ChannelService = require("../services/channel.services");
+const { generateResponse } = require("../utils/common.utils");
 
 const sendMessage = async (req, res) => {
-    let { message } = req.body
-    try {
-        let result = await ChannelService.sendMessage(message)
-        return res.status(result.code).send(generateResponse(result.success, result.message, result.data, result.code))
-    } catch (error) {
-        return res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).send(generateResponse(false, SERVER_RESPONSES.INTERNAL_SERVER_ERROR, { error: error.message }, HTTP_CODES.INTERNAL_SERVER_ERROR))
-    }
-}
-
+  let { message } = req.body;
+  try {
+    let result = await ChannelService.sendMessage(message);
+    return res
+      .status(result.code)
+      .send(
+        generateResponse(
+          result.success,
+          result.message,
+          result.data,
+          result.code
+        )
+      );
+  } catch (error) {
+    logger.error(`Failed to send message error: ${error.message}`);
+    return res
+      .status(HTTP_CODES.INTERNAL_SERVER_ERROR)
+      .send(
+        generateResponse(
+          false,
+          SERVER_RESPONSES.INTERNAL_SERVER_ERROR,
+          { error: error.message },
+          HTTP_CODES.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
 
 module.exports = {
-    sendMessage
-}
+  sendMessage,
+};
--- src/utils/mailUtilities/mailingStrategies/mailjetMailingStrategy.js
+++ src/utils/mailUtilities/mailingStrategies/mailjetMailingStrategy.js
@@ -1,115 +1,133 @@
 const { MailingStrategy } = require("./mailingStrategies");
-const config = require("../../../../config.json")
-const Mailjet = require('node-mailjet');
-const { MESSAGE_LISTENER_INTERNAL_RESPONSES } = require("../../../constants/messangingChannel.constant");
+const config = require("../../../../config.json");
+const Mailjet = require("node-mailjet");
+const {
+  MESSAGE_LISTENER_INTERNAL_RESPONSES,
+} = require("../../../constants/messangingChannel.constant");
 const { resultObject } = require("../../common.utils");
 const { HTTP_CODES } = require("../../../constants/httpCodes.constant");
 const { logger } = require("../../../di-container");
 const mailjet = new Mailjet({
-    apiKey: config.MAILING_TOOLS.MAILJET.API_KEY,
-    apiSecret: config.MAILING_TOOLS.MAILJET.API_KEY_SECRET,
-})
-
+  apiKey: config.MAILING_TOOLS.MAILJET.API_KEY,
+  apiSecret: config.MAILING_TOOLS.MAILJET.API_KEY_SECRET,
+});
 
 class MailjetMailingStrategy extends MailingStrategy {
-    initialize() {
+  initialize() {}
 
+  validateProvidedBodyForSendingMail(mailjetData) {
+    if (!mailjetData) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_CORE_DATA_FOR_MAILJET_MAILING_STRATEGY
+      );
+    }
+    let { from, to, subject, TextPart, HTMLPart } = mailjetData;
+
+    if (!from) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_FROM_KEY_FROM_MAILJET_PROVIDED_DATA
+      );
+    }
+    if (!to) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_TO_KEY_FROM_MAILJET_PROVIDED_DATA
+      );
+    }
+    if (!subject) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_SUBJECT_KEY_FROM_MAILJET_PROVIDED_DATA
+      );
+    }
+    if (!HTMLPart && !TextPart) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.NITHER_TEXT_PART_NOR_HTML_PART_KEY_PROVIDED_IN_MAILJET_PROVIDED_DATA
+      );
     }
 
-    validateProvidedBodyForSendingMail(mailjetData) {
-        if (!mailjetData) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_CORE_DATA_FOR_MAILJET_MAILING_STRATEGY)
-        }
-        let {
-            from,
-            to,
-            subject,
-            TextPart,
-            HTMLPart,
-        } = mailjetData
-
-        if (!from) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_FROM_KEY_FROM_MAILJET_PROVIDED_DATA)
-        }
-        if (!to) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_TO_KEY_FROM_MAILJET_PROVIDED_DATA)
-        }
-        if (!subject) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.MISSING_SUBJECT_KEY_FROM_MAILJET_PROVIDED_DATA)
-        }
-        if (!HTMLPart && !TextPart) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.NITHER_TEXT_PART_NOR_HTML_PART_KEY_PROVIDED_IN_MAILJET_PROVIDED_DATA)
-        }
-
-        if (!from?.email) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.IN_FROM_KEY_IN_MAILJET_PROVIDED_DATA_EMAIL_IS_MISSING)
-        }
-
-        if (!to?.email) {
-            throw new Error(MESSAGE_LISTENER_INTERNAL_RESPONSES.IN_TO_KEY_IN_MAILJET_PROVIDED_DATA_EMAIL_IS_MISSING)
-        }
+    if (!from?.email) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.IN_FROM_KEY_IN_MAILJET_PROVIDED_DATA_EMAIL_IS_MISSING
+      );
     }
-    async sendMail({
-        mailjetData,
-        message
-    }) {
-        return new Promise((resolve, reject) => {
-            const messageKey = message.key.toString()
 
-            this.validateProvidedBodyForSendingMail(mailjetData)
-            logger.info(`Validated Provided body for sending mail for message: ${messageKey}`, {
-                mailjetData: mailjetData
-            })
-            let {
-                from,
-                to,
-                subject,
-                TextPart,
-                HTMLPart,
-            } = mailjetData
-            const request = mailjet
-                .post('send', { version: 'v3.1' })
-                .request({
-                    Messages: [
-                        {
-                            From: {
-                                Email: from?.email,
-                                ...(from?.name ? { Name: from?.name } : {})
-                            },
-                            To: [
-                                {
-                                    Email: to?.email,
-                                    ...(to?.name ? { Name: to?.name } : {})
-                                }
-                            ],
-                            Subject: subject,
-                            ...(TextPart ? { TextPart: TextPart } : {}),
+    if (!to?.email) {
+      throw new Error(
+        MESSAGE_LISTENER_INTERNAL_RESPONSES.IN_TO_KEY_IN_MAILJET_PROVIDED_DATA_EMAIL_IS_MISSING
+      );
+    }
+  }
+  async sendMail({ mailjetData, message }) {
+    return new Promise((resolve, reject) => {
+      const messageKey = message.key.toString();
 
-                            ...(HTMLPart ? { HTMLPart: HTMLPart } : {})
-                        }
-                    ]
-                })
+      this.validateProvidedBodyForSendingMail(mailjetData);
+      logger.info(
+        `Validated Provided body for sending mail for message: ${messageKey}`,
+        {
+          mailjetData: mailjetData,
+        }
+      );
+      let { from, to, subject, TextPart, HTMLPart } = mailjetData;
+      const request = mailjet.post("send", { version: "v3.1" }).request({
+        Messages: [
+          {
+            From: {
+              Email: from?.email,
+              ...(from?.name ? { Name: from?.name } : {}),
+            },
+            To: [
+              {
+                Email: to?.email,
+                ...(to?.name ? { Name: to?.name } : {}),
+              },
+            ],
+            Subject: subject,
+            ...(TextPart ? { TextPart: TextPart } : {}),
 
-            request
-                .then((result) => {
-                    logger.info(`Received response for mailjet for message: ${messageKey}`, {
-                        mailjetResponse: result.body
-                    })
-                    return resolve(resultObject(true, `Successfully sent mail`, {
-                        responseBody: result.body,
-                    }, HTTP_CODES.OK))
-                })
-                .catch((err) => {
-                    logger.error(`Error: Received response for mailjet for message: ${messageKey}`, {
-                        error: err
-                    })
-                    return resolve(resultObject(false, `Failed to sent mail`, {
-                        error: err,
-                    }, HTTP_CODES.INTERNAL_SERVER_ERROR))
-                })
+            ...(HTMLPart ? { HTMLPart: HTMLPart } : {}),
+          },
+        ],
+      });
+
+      request
+        .then((result) => {
+          logger.info(
+            `Received response for mailjet for message: ${messageKey}`,
+            {
+              mailjetResponse: result.body,
+            }
+          );
+          return resolve(
+            resultObject(
+              true,
+              `Successfully sent mail`,
+              {
+                responseBody: result.body,
+              },
+              HTTP_CODES.OK
+            )
+          );
         })
-
-    }
+        .catch((err) => {
+          logger.error(
+            `Error: Received response for mailjet for message: ${messageKey}`,
+            {
+              error: err,
+            }
+          );
+          return resolve(
+            resultObject(
+              false,
+              `Failed to sent mail`,
+              {
+                error: err,
+              },
+              HTTP_CODES.INTERNAL_SERVER_ERROR
+            )
+          );
+        });
+    });
+  }
 }
 
-module.exports = new MailjetMailingStrategy()
+module.exports = new MailjetMailingStrategy();
